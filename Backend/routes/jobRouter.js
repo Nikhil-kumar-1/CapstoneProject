be23fc53c9a1b8f034fd2e5e6687fbbc9ba936a9
Nikhil <kumarnikhil787031@gmail.com>
2024-03-13 00:16:05 +0530

    third commit

--- Backend/routes/jobRouter.js
+++ Backend/routes/jobRouter.js
@@ -1,5 +1,13 @@
 import express from "express";
-
+import {
+  getAllJobs,
+  getMyJobs,
+  postJob,
+} from "../controllers/jobController.js";
+import { isAuthorized } from "../middlewares/auth.js";
 const router = express.Router();
 
+router.get("/getAll", getAllJobs);
+router.post("/post", isAuthorized, postJob);
+router.get("/getmyjobs", isAuthorized, getMyJobs);
 export default router;
